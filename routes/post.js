5c660f32eaf19fcbe6119038de800c2f03a6614b
Vithursan6 <93847590+Vithursan6@users.noreply.github.com>
2022-04-04 00:49:35 -0400

    added update, delete for posts, formhandling and content handling for user

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getPosts, createPost } = require("../controllers/post");
+const { getPosts, createPost, postsByUser, postById, isPoster, deletePost, updatePost } = require("../controllers/post");
 const { requireSignin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 const { createPostValidator } = require("../validator");
@@ -7,9 +7,20 @@ const { createPostValidator } = require("../validator");
 const router = express.Router();
 
 router.get("/", getPosts);
-router.post("/post", requireSignin, createPostValidator, createPost);
+router.post(
+    "/post/new/:userId",
+     requireSignin,
+     createPost,
+     createPostValidator
+
+     
+);
+router.get("/posts/by/:userId", requireSignin, postsByUser);
+router.put("/post/:postId", requireSignin, isPoster, updatePost);
+router.delete("/post/:postId", requireSignin, isPoster, deletePost);
 
 router.param("userId", userById);
+router.param("postId", postById);
 
 
 module.exports = router;
